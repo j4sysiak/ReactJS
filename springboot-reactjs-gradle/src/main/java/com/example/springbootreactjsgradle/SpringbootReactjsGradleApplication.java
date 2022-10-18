521a875fb092e85ff0affba0a95623d386c436c1
Jacek Sysiak <j4sysiak@gmail.com>
2022-10-18 07:18:15 +0200

    18-10-2022 07:17

---
+++ springboot-reactjs-gradle/src/main/java/com/example/springbootreactjsgradle/SpringbootReactjsGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactjsgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactjsGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactjsGradleApplication.class, args);
+	}
+
+}
