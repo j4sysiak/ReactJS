9cf06eaffd2c7ad318d7f799462d088d1037728e
Jacek Sysiak <j4sysiak@gmail.com>
2022-10-24 19:05:16 +0200

    24-10-2022 19:04

--- springboot-reactjs-gradle-4/src/frontend/myreactapp/src/App.js
+++ springboot-reactjs-gradle-4/src/frontend/myreactapp/src/App.js
@@ -1,26 +1,64 @@
 import React, {useState} from 'react';
 import './App.css';
 import PlanetList from "./PlanetList/PlanetList";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+    BrowserRouter,
+    Routes, // instead of "Switch"
+    Route,
+    NavLink
+} from "react-router-dom";
 
 function App() {
 
     const [shouldShowState, shouldShowStateUpdate] = useState(true);
-    const planetContent = shouldShowState === true ? (<div><PlanetList/></div>) : null;
+    // const planetContent = shouldShowState === true ? (<div><PlanetList/></div>) : null;
 
     return (
         <BrowserRouter>
             <div className="App">
                 <div className='navigationTop'>
-                    <div className='navItem'><a href='/planets'>Planets</a></div>
-                    <div className='navItem'><a href='/quote'>Quotes</a></div>
-                    <div className='navItem'><a href='/spaceship'>Spaceship</a></div>
+                    <div className='navItem'>
+                        <NavLink to='/planets'>Planets</NavLink>
+                    </div>
+                    <div className='navItem'>
+                        <NavLink to='/quotes'>Quotes</NavLink>
+                    </div>
+                    <div className='navItem'>
+                        <NavLink to='/asteroids'>Asteroids</NavLink>
+                    </div>
                 </div>
-                {planetContent}
+                <Routes>
+                    <Route path="/planets" element={ <PlanetList /> }/>
+                    <Route path='/quotes' element = {<h1>Quotes</h1>}/>
+                    <Route path='/asteroids' element = {<h1>Asteroids!!!</h1>}/>
+                    <Route path='/' element = {<h1>(default error when path is empty: ) Unknown path!!!!!!</h1>}/>
+                </Routes>
             </div>
-
         </BrowserRouter>
     );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
